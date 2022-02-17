e515c7ac4cd722d0f2159c04b74cb7d739e5deea
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-17 23:50:01 +0530

    Length_Of_Line

--- Program.cs
+++ Program.cs
@@ -3,9 +3,20 @@ namespace linesProblemfinal
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Welcome to the line length calculation problems");
+            Console.Write("Value of the X1:");
+            double valueOfX1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Value of the X2:");
+            double valueOfY1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Value of the Y1:");
+            double valueOfX2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Value of the Y2:");
+            double valueOfY2 = Convert.ToDouble(Console.ReadLine());
+            double lengthOfALine = (int)(Math.Pow(valueOfX2 - valueOfY1, 2) + Math.Pow(valueOfX2 + valueOfY1, 2));
+            
+            Console.WriteLine(lengthOfALine);
+
         }
     }
 }
