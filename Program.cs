8ad1a0d60c844d799c40d49fcb2a8f16aba455ff
Gangadevi Ch <chilamakooru.gangaiiit@gmail.com>
2022-02-17 23:41:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace linesProblemfinal
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the line length calculation problems");
+        }
+    }
+}
