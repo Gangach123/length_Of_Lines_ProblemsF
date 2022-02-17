4eef02253ed8900aa785a847def18337d3d2ddb4
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-18 00:23:16 +0530

    Lines_Comparision

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+
 namespace linesProblemfinal
 {
     class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Co-ordinates of 1st Line");
+            double line1Length = Main2();
+            Console.WriteLine("Enter Co-ordinates of 2nd Line");
+            double line2Length = Main2();
+            string result = line1Length.Equals(line2Length) == true ? "Two lines are equal" : "Two lines are not equal";
+            Console.WriteLine(result);
+        }
+        public static double Main2()
         {
             Console.Write("Value of the X1:");
             double valueOfX1 = Convert.ToDouble(Console.ReadLine());
@@ -13,9 +23,10 @@ namespace linesProblemfinal
             double valueOfX2 = Convert.ToDouble(Console.ReadLine());
             Console.Write("Value of the Y2:");
             double valueOfY2 = Convert.ToDouble(Console.ReadLine());
-            double lengthOfALine = (int)(Math.Pow(valueOfX2 - valueOfY1, 2) + Math.Pow(valueOfX2 + valueOfY1, 2));
+            double lengthOfALine =Math.Pow(Math.Pow(valueOfX2 - valueOfY1, 2) + Math.Pow(valueOfX2 + valueOfY1, 2),0.5);
+            return lengthOfALine;
             
-            Console.WriteLine(lengthOfALine);
+            //Console.WriteLine(lengthOfALine);
 
         }
     }
